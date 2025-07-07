a578637f28b59405a67e86b1bf0724d6f3e76611
SubahaSalman <subaha.salman@gmail.com>
2025-07-07 19:12:48 +0200

    swapping numbers

---
+++ swapping numbers.py
@@ -0,0 +1,31 @@
+from random import randint
+
+
+a=5
+b=10
+c=20
+
+if a==5:
+    a=b
+
+print(a)
+
+a=5
+b=10
+c=20
+
+if b==10:
+    b=c
+
+print(b)
+
+a=5
+b=10
+c=20
+
+if c==20:
+    c=a
+
+print(c)
+
+
