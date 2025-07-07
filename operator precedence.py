0a90deb601428e1a2768023c2df646bcb183f97c
SubahaSalman <subaha.salman@gmail.com>
2025-07-07 18:45:42 +0200

    first commit

---
+++ operator precedence.py
@@ -0,0 +1,16 @@
+v=6
+w=5
+x=3
+y=9
+z=0
+
+z= (v+w) * x / y
+print("Value of (v+w) * x / y is", z)
+
+name = "Alex"
+age = 0
+
+if name == "Alex" or name == "John" and age>=2:
+    print("Hello!")
+else:
+    print("Goodbye!")
