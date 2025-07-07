1eee552de39ddc792fa18cf9e3a37197b645110f
SubahaSalman <subaha.salman@gmail.com>
2025-07-07 19:01:10 +0200

    mean value

---
+++ mean value.py
@@ -0,0 +1,12 @@
+mean1= 35
+wrong_number=13
+correct_number=47
+total_number=80
+sum= mean1 * total_number
+print("The sum of 80 number = ", sum)
+
+num2 = sum-((wrong_number) -(correct_number))
+print("sum-((wrong_number) -(correct_number))= ", num2)
+
+mean2=num2/total_number
+print(mean2)
